873655b407964078a3d2a8918968abe716a80ced
Rennorb <18741506+SaculRennorb@users.noreply.github.com>
2024-06-29 05:41:01 +0200

    moving things around slightly
    extending the basic header instead of adding a field is a bit cleaner imo, but now this should basically be the final state of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,13 @@
 #include <stdint.h>
 
 #define MY_ADDON_ID  -1234
+//NOTE(Rennorb): We will set these in the Load event:
+// Send function: `bool PrepareAndBroadcastPacket(Packet* packet)`
+PacketPrepareAndBroadcast* PrepareAndBroadcastPacket = 0;
+LOGGER_LOG2 Log = 0;
 #define _LOG(lvl, fmt, ...) Log(ELogLevel::ELogLevel_ ## lvl, "PingPongAddon", fmt, __VA_ARGS__)
+
+
 
 enum PacketType : uint16_t {
 	_UNKNOWN = 0,
@@ -11,35 +17,52 @@ enum PacketType : uint16_t {
 	Pong = 2,
 };
 
+// Extend the basic header.
+struct MyPacketHeader : PacketHeader {
+	// If we want to have multiple packets, we need to differentiate them somehow.
+	PacketType PacketType;
+};
+static_assert(sizeof(MyPacketHeader) == 8);
+
 // Create a custom Packet struct you want to send:
-#pragma pack(push, 1)
 struct MyPacket {
-	PacketHeader Header;
-	PacketType PacketType; // if we want to have multiple packets, we need to differentiate them somehow
+	MyPacketHeader Header;
 
 	uint32_t Payload; // data we want to send
 
-	PacketChecksum CRC; // just for sizing, we dont touch that
+	PacketChecksum CRC; // Just for sizing, we don't touch that
 };
-#pragma pack(pop)
 
-// Send function: `bool PrepareAndBroadcastPacket(Packet* packet)`
-PacketPrepareAndBroadcast* PrepareAndBroadcastPacket = 0;
-LOGGER_LOG2 Log = 0;
+
+
+void ProcessMyKeybinds(const char* aIdentifier, bool aIsRelease)
+{
+	if(aIsRelease) return; // only on key press
+
+	MyPacket ping {};
+	ping.Header.TargetAddon = MY_ADDON_ID,
+	ping.Header.LengthInU32s = PACKET_LEN(ping),
+	ping.Header.PacketType = PacketType::Ping,
+
+	ping.Payload = 1; // set some random payload
+
+	PrepareAndBroadcastPacket((Packet*)&ping);
+	_LOG(TRACE, "Ping sent");
+}
 
 void HandleIncomingPacket(Packet const* _packet)
 {
 	auto packet = (MyPacket const*)_packet;
-	switch(packet->PacketType) {
+	switch(packet->Header.PacketType) {
 	case PacketType::Ping: {
 		_LOG(INFO, "ping received");
 
-		MyPacket response = {0};
+		MyPacket response {};
 		response.Header.TargetAddon = MY_ADDON_ID;
 		response.Header.LengthInU32s = PACKET_LEN(response);
-		response.PacketType = PacketType::Pong;
+		response.Header.PacketType = PacketType::Pong;
 
-		response.Payload = packet->Payload;
+		response.Payload = packet->Payload; //copy over the payload
 
 		PrepareAndBroadcastPacket((Packet*)&response);
 		_LOG(TRACE, "pong sent");
@@ -57,21 +80,6 @@ void HandleIncomingPacket(Packet const* _packet)
 	}
 }
 
-
-void ProcessMyKeybinds(const char* aIdentifier, bool aIsRelease)
-{
-	if(aIsRelease) return; // only on key press
-
-	MyPacket ping = {0};
-	ping.Header.TargetAddon = MY_ADDON_ID;
-	ping.Header.LengthInU32s = PACKET_LEN(ping);
-	ping.PacketType = PacketType::Ping;
-
-	ping.Payload = 1;
-
-	PrepareAndBroadcastPacket((Packet*)&ping);
-	_LOG(TRACE, "Ping sent");
-}
 
 
 // Use the load function to get a function for sending packets, and to install a callback for receiving packets
